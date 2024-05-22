d384eee5a4ba8f7978362ea5c291a63cc7651107
SriSatPan <satish.periche@gmail.com>
2024-05-22 15:57:38 +0530

    new code mofidfied on slave1

--- src/Pages/CartPageTest.java
+++ src/Pages/CartPageTest.java
@@ -5,6 +5,7 @@ public class CartPageTest {
     public static void main(String[] args) {
 
         System.out.println("Cart Test Page!");
+        System.out.println("Cart Test Page Modified!");
 
     }
 }
