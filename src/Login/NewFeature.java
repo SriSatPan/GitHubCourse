5f5d98fe2b41563cc2a20fbfa4f6d0d70ee46cd7
SriSatPan <satish.periche@gmail.com>
2024-05-25 12:05:44 +0530

    new featured done

---
+++ src/Login/NewFeature.java
@@ -0,0 +1,11 @@
+package Login;
+
+public class NewFeature {
+
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("New Feature!!");
+
+    }
+}
