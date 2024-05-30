53aa010647de38d35728671264bddcd0fb6f1ea4
SriSatPan <satish.periche@gmail.com>
2024-05-30 17:14:08 +0530

    second cherry pick

--- src/Pages/CherryPick.java
+++ src/Pages/CherryPick.java
@@ -6,5 +6,6 @@ public class CherryPick {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Cherry Pick!!");
+        System.out.println("cherry pick2!!");
     }
 }
