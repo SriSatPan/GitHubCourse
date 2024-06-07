6af827c394a08a13dd22b8de59e4d66b6cd49499
SriSatPan <169049163+SriSatPan@users.noreply.github.com>
2024-06-07 15:45:53 +0530

    Create Classicpage.java

---
+++ src/Login/Classicpage.java
@@ -0,0 +1,13 @@
+package Login;
+
+public class ClassicPage {
+
+    public void classic() {
+
+        int K =10;
+        int K1 = 20;
+        int p = K+K1;
+        System.out.println(p);
+
+    }
+}
